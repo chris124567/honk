47380249d3435b542e0e974777e192788ba54b06
Chris <>
2020-01-26 18:30:20 -0500

    add description to python script

--- changebyte.py
+++ changebyte.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+This script prevents gdb from recognizing the file as an executable, but Linux is still able to run it.
+"""
+
 import io
 import mmap
 with open("server", "r+b") as f:
